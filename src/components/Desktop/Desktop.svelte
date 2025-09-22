0ed4eb4b9f916091953c4fd2daf1f1218e83488f
skibidi_toilet <m.pantsenkov@hotmail.com>
2025-09-22 21:07:47 +0100

    Update Desktop.svelte

--- src/components/Desktop/Desktop.svelte
+++ src/components/Desktop/Desktop.svelte
@@ -13,7 +13,7 @@
 		Promise.all([
 			import('@fontsource/inter/latin-ext-300.css'),
 			import('@fontsource/inter/latin-ext-400.css'),
-			import('@fontsource/inter/latin-ext-500.css'),
+			import('@fontsource/inter/latin-ext-500.css'),jhyhtrhdfcndtrd
 			import('@fontsource/inter/latin-ext-600.css'),
 		]);
 	}
